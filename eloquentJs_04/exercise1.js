bd58ffcb0893a468397e1b5ec685db0e3fffc79d
lecsven <lecsven@gmail.com>
2018-01-04 21:52:42 +0800

    lecsven

--- eloquentJs_04/exercise1.js
+++ eloquentJs_04/exercise1.js
@@ -1,8 +1,8 @@
 function range(start,end,step){
   var string=[];
-  console.log(arguments.length);
+  
   if (arguments.length == 2)
-    step == 1;
+    step = 1;
   if (start < end){
     for(count=start;count<=end;count+=step){
       string.push(count);
@@ -17,12 +17,18 @@ function range(start,end,step){
 }
   return string
 }
-console.log(range(2, 10));
+function sum(range){
+  counted = 0;
+  for(var ch in range){
+    counted +=range[ch];
+  }
+  return counted;
+}
+console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-console.log(range(1, 20,2));
-console.log(range(50, 20, -3));
 console.log(sum(range(1, 10)));
 // → 55
-console.log(range(1, 20));
+console.log(range(1, 20,2));
+console.log(range(50, 20, -3));
